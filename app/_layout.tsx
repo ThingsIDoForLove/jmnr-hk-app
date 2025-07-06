86f004e25a2e71338dee3f5ef7a836b22683f3d1
Rasikh <rasikh.mashahdi@supermetrics.com>
2025-07-06 18:58:04 +0300

    remove sentry

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,22 +4,11 @@ import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
-import * as Sentry from '@sentry/react-native';
 import * as SecureStore from 'expo-secure-store';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import LoginScreen from '../components/LoginScreen';
 import HomeScreen from './index';
 
-Sentry.init({
-  dsn: 'https://5b9646d9b1c37d756ebe59967358647d@o4509622034694144.ingest.de.sentry.io/4509622036725840',
-
-  // Adds more context data to events (IP address, cookies, user, etc.)
-  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
-  sendDefaultPii: true,
-
-  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-  // spotlight: __DEV__,
-});
 
 export default Sentry.wrap(function RootLayout() {
   const colorScheme = useColorScheme();
